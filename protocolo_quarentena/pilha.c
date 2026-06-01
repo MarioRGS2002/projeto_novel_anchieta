f694e1a6383a0c5f5c7e6c77f8e4e6ac74d5a09f
Mario Romero <marioromero1.pa@hotmail.com>
2026-06-01 18:11:39 -0300

    Update pilha.c

--- protocolo_quarentena/pilha.c
+++ protocolo_quarentena/pilha.c
@@ -10,7 +10,7 @@ void pilha_inicializar(Pilha *p) {
 void pilha_empilhar(Pilha *p, int id_cena) {
     NoPilha *novo = (NoPilha *)malloc(sizeof(NoPilha));
     if (novo == NULL) {
-        printif("EWrro: memoria insuficiente!\n");
+        printif("Erro: memoria insuficiente!\n");
         return;
     }
     novo->id_cena = id_cena;
@@ -18,3 +18,17 @@ void pilha_empilhar(Pilha *p, int id_cena) {
     p->topo = novo;
     p->tamanho++;
 }
+
+int pilha_desempilhar(Pilha *p) {
+    if (pilha_vazia(p)) {
+        return -1;
+
+    }
+    NoPilha *removido = p->topo;
+    int id = removido->id_cena;
+    p->topo = removido ->anterior;
+    free(removido);
+    p->tamanho--;
+    return id;
+}
+
